f2e929bb0ea20b0f7d77b21e578cf25162355562
Hardy Abreu <harvix@ChloeSoftDevLogiciels.local>
2015-05-28 15:32:44 -0400

    grupo_61.py

---
+++ grupo_61.py
@@ -0,0 +1,11 @@
+from cli import *
+cli ("conf ; int e1/2 ; no switchport; ip address 192.168.61.1/24 ; no shut")
+cli ("conf ; vlan 1005")
+cli ("conf ; int e1/3 ; switchport; switchport access vlan 1005 ; no shut")
+clip ("show run int e1/2")
+raw_input('press Enter to continue')
+clip ("show run int e1/3")
+raw_input('press Enter to continue')
+clip ("show run int brief")
+raw_input('press Enter to continue')
+clip ("show ip int brief")
